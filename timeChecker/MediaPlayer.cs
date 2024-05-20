2e0e7376c94e2a07d490ab6ca52c8ae763c1d7ce
Scathak <scathakocridera@gmail.com>
2024-05-20 19:30:09 +0300

    Create MediaPlayer.cs
    MediaPlayer file added

---
+++ timeChecker/MediaPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Media;
+using System.IO;
+
+namespace timeChecker
+{
+    public class AlarmPlayer(string fileName)
+    {
+        private readonly string _fileName = fileName;
+
+        public void StartAlarm()
+        {
+            var theread1 = new Thread(new ThreadStart(NewAlarm));
+            theread1.Start();
+        }
+        private void NewAlarm()
+        {
+            var uri = new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory
+                , "wav"
+                , _fileName));
+            var player = new MediaPlayer();
+            player.Open(uri);
+            player.Play();
+        }
+    }
+}
